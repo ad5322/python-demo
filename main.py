85ca764127b98241cd4ccf02dde3a743efd505f8
ad5322 <arundeepak966@gmail.com>
2024-10-09 13:26:00 +0530

    abs

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from pprint import pprint
+from abc import ABC
 
 
-class Employee:
-    def __init__(self, name,base_pay,bonus) -> None:
-        self.name = name
-        self.base_pay = base_pay
-        self.bonus = bonus
+class Point2D:
+    __slots__ = ('x','y')
     
-    def get_pay(self):
-        return self.base_pay + self.bonus    
-
-
-
-
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        
+    def __repr__(self):
+        return f'Piont2D {self.x} {self.y}'
+    
+    
+class Point3D(Point2D):
+    __slots__ = ('z',)
+    def __init__(self, x,y,z):
+        super().__init__(x, y)
+        self.z = z
+        
+if __name__ == '__main__':
+    point = Point3D(1,2,3)
+    pprint(point.__slots__)
+    
 
