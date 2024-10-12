2d5d8ae333127020ba31720180ee963a4e3afa3a
ad5322 <arundeepak966@gmail.com>
2024-10-12 14:06:03 +0530

    class_demo

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
-class Person:
-    def __init__(self, first_name, last_name, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-    
-    def __repr__(self) -> str:
-        return f'person {self.first_name} {self.last_name} {self.age}'
-    
-    def __eq__(self, other) -> bool:
-         return self.age == other.age , self.last_name == other.last_name , \
-         self.first_name == other.first_name
-         
-    
+import keyword
+from pprint import pprint
 
-                
-                
+def main():
+    i = 1
+    max = 10
+    while(i <= 10):
+        print(i)
+        i += 1
         
-arun  = Person('arun','deepak',31)
-kali = Person('kali','deepak',31)
-
-print(arun == kali)
-
-
-
+my_name = 'arun'
+my_name = "deepak"
+my_name = """Hello world hello is there anything"""
+        
+pprint(my_name)
