553bd4a5656ec4a0ccadea4b4befeffd474e54c7
ad5322 <arundeepak966@gmail.com>
2024-10-14 08:11:36 +0530

    comparison operators

--- class_demo.py
+++ class_demo.py
@@ -1,26 +1,14 @@
-import math
-class Person():
-    counter = 0
-    def __init__(self,name,age):
-        self.name = name
-        self.age = age
-        Person.counter += 1
-        
-    def greet(self):
-        return f'Hello {self.name}'
-    
-    @classmethod
-    def create_anonnymous(cls):
-        return Person('deepak',99)
+from pprint import pprint
 
-class TempratureConverter():
-    @staticmethod
-    def Celsius_to_fahrenheit(c):
-        return 9 * c / 5 + 32
+class Person:
+    pass
     
-    @staticmethod
-    def fahrenheit_to_Celsius(f):
-        return 5 * (f - 32) / 9
-    
-f = TempratureConverter.fahrenheit_to_Celsius(100)
-print(math.ceil(f))
+    # def __init__(self,name,age):
+    #     self.name = name
+    #     self.age = age
+        
+if __name__ == '__main__':
+    # x = Person('arun',78)
+    x = Person()
+    # print(f'{x.name} {x.age}')
+    print(x.__dict__)
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import keyword
 from pprint import pprint
+import random
 
-def main():
-    i = 1
-    max = 10
-    while(i <= 10):
-        print(i)
-        i += 1
-        
-message = 'Good Morning'
-print(message)
+# price = input('enter net price ($) \n')
+# tax = input('enter net tax (%) \n')
+
+# tax_amount = int(price) * int(tax) / 100
+
+# print(f'the tax amount is {tax_amount}% ')
+
+
+x = random.randrange(1,100)
+y = int(input('Enter a number'))
+
