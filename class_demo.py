d26230da0fa67ad688b176a817b4d5bb33126df1
ad5322 <arundeepak966@gmail.com>
2024-10-20 08:42:52 +0530

    functions and __eq__ method

--- class_demo.py
+++ class_demo.py
@@ -1,17 +1,21 @@
 from pprint import pprint
 
-class Person():
+class Person:
     def __init__(self,f_name,l_name,age):
         self.first_name = f_name
         self.last_name = l_name
         self.age = age
-        
-    def __str__(self):
-        return f'Person - {self.first_name} {self.last_name} {self.age}'
     
-    # def __repr__(self):
-    #     return f'{self.first_name} '
-person  = Person('arun','deepak',31)
-print(person)
-print(repr(person))
-print(person)
+    def __eq__(self, value):
+        if isinstance(value,Person):
+            return self.age == value.age
+        return False
+    def __eq__(self, value):
+         if isinstance(value,Person):
+            return self.last_name == value.last_name
+         return False    
+     
+john = Person('john','doe',25)
+jane = Person('jane','doe',27)
+print(john == jane)
+    
--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import keyword
 from pprint import pprint
 import random
 
-counter = 0
-while counter < 10:
-    counter += 1
-    if not counter % 2:
-        continue
-    print(counter)
+def sum(a,b):
+    total = a + b
+    return total
+x = sum(10,20)
+print(x)
