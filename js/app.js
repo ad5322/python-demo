f0f4408409931e0829b56f4e65e1050a0d86dc85
ad5322 <arundeepak966@gmail.com>
2024-11-08 17:07:35 +0530

    obj

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
-let formatted = true;
 
-if(formatted){
-    console.log('Code is easy to Read');
-}
+let Person = {
+    firstName:'Arun',
+    lastName:'Deepak',
+    building_No:'5/152, Kmarajar street',
+    employeeId:1,
+};
+
+Person.firstName = 'Karan';
+Person.age = 31;
+console.log(Person.firstName)
+console.log(Person['lastName'])
+console.log('employeeId' in Person)
+console.log('price' in Person)
